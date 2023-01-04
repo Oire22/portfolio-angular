e50c4facc0582d347ee3c33e4bc421bf02ef6a48
Oire22 <oliveriomaranesi@gmail.com>
2023-01-04 16:18:39 -0300

    portfolio angular 04/01

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,28 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+//mis componentes
+
+import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './header/header.component';
+import { AboutComponent } from './about/about.component';
+import { ExperienceComponent } from './experience/experience.component';
+import { SkillsComponent } from './skills/skills.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { ContactComponent } from './contact/contact.component';
+import { FooterComponent } from './footer/footer.component';
+
+const routes: Routes = [
+  {path: '', redirectTo: "/home", pathMatch: 'full'},
+  {path: "home", component: HomeComponent },
+  {path: "header", component: HeaderComponent },
+  {path: "about", component: AboutComponent },
+  {path: "experience", component: ExperienceComponent },
+  {path: "skills", component: SkillsComponent },
+  {path: "projects", component: ProjectsComponent },
+  {path: "contact", component: ContactComponent },
+  {path: "footer", component: FooterComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './header/header.component';
+import { AboutComponent } from './about/about.component';
+import { ExperienceComponent } from './experience/experience.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { FooterComponent } from './footer/footer.component';
+import { SkillsComponent } from './skills/skills.component';
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    HeaderComponent,
+    AboutComponent,
+    ExperienceComponent,
+    ProjectsComponent,
+    FooterComponent,
+    SkillsComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
